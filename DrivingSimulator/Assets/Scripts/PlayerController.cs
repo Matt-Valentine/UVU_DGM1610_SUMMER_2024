6a49744188421ce9bee1ba9b30f0d7b43ebb67c4
Matthew Valentine <mattvalentine19@gmail.com>
2024-06-20 12:50:53 -0600

    Added Player Controller and FollowPlayer scripts back

--- DrivingSimulator/Assets/Scripts/PlayerController.cs
+++ DrivingSimulator/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 25.0f;
+
+    private float turnSpeed = 75.0f;
+
+    public float horizontalInput;
+
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,7 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Will move the vehicle forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
+        // Rotates the car
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
