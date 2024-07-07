df06d8252263b55ff60c8c016a1282d99adc915d
Matthew Valentine <mattvalentine19@gmail.com>
2024-07-07 17:15:15 -0600

    Completed Play Fetch Challenge

---
+++ DAGV 1400 Unity/PlayFetchChallenge/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveForwardX : MonoBehaviour
+{
+    public float speed = 45.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+}
---
+++ DAGV 1400 Unity/PlayFetchChallenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCoolDown = 0.5f;
+    private float lastSpawnTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastSpawnTime + spawnCoolDown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = Time.time;
+        }
+    }
+}
