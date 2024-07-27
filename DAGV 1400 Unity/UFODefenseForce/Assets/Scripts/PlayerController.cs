8cce9692628116a7a7684118c4472b7d27b0850f
Matthew Valentine <mattvalentine19@gmail.com>
2024-07-26 18:55:33 -0600

    UFO Defense Force scripts/Pick-up item challenge

--- DAGV 1400 Unity/UFODefenseForce/Assets/Scripts/PlayerController.cs
+++ DAGV 1400 Unity/UFODefenseForce/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed;
-    
+    public float speed = 20.0f;
+    public float xRange = 12;
+    public Transform blaster;
+
+    public GameObject blasterBolt;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +21,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Moves player left/right
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+        // Player Boundary Left Side
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Player Boundary Right Side
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create blasterBolt at blaster location
+            Instantiate(blasterBolt, blaster.transform.position, blasterBolt.transform.rotation);
+        }
+    }
+
+    // Delete objects that collide with player
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
